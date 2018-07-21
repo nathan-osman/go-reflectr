e127c236d195d5664fe8dd69909a8868b8b1b0ea
Nathan Osman <nathan@quickmediasolutions.com>
2018-07-20 23:47:27 -0700

    Add ability to check return values.

--- struct.go
+++ struct.go
@@ -14,8 +14,13 @@ const (
 )
 
 var (
-	errMustBeStruct       = errors.New("parameter must be a struct or pointer to struct")
+	errMustBeStruct = errors.New("parameter must be a struct or pointer to struct")
+
 	errMethodDoesNotExist = errors.New("method does not exist")
+	errMethodNotSelected  = errors.New("method not selected")
+
+	errReturnsCount = errors.New("return value count mismatch")
+	errReturnsType  = errors.New("return value type mismatch")
 )
 
 // StructMeta provides methods for struct introspection.
@@ -54,9 +59,36 @@ func (s *StructMeta) Method(name string) *StructMeta {
 	}
 	m, ok := s.structType.MethodByName(name)
 	if ok {
+		s.selType = stMethod
 		s.method = m
 	} else {
 		s.err = errMethodDoesNotExist
+	}
+	return s
+}
+
+// Returns ensures that the return types of a method matche the provided types.
+func (s *StructMeta) Returns(v ...interface{}) *StructMeta {
+	if s.err != nil {
+		return s
+	}
+	if s.selType != stMethod {
+		s.err = errMethodNotSelected
+		return s
+	}
+	var (
+		methodType = s.method.Type
+		paramCount = methodType.NumOut()
+	)
+	if len(v) != paramCount {
+		s.err = errReturnsCount
+		return s
+	}
+	for i := 0; i < paramCount; i++ {
+		if reflect.TypeOf(v[i]) != methodType.Out(i) {
+			s.err = errReturnsType
+			return s
+		}
 	}
 	return s
 }
--- struct_test.go
+++ struct_test.go
@@ -9,6 +9,10 @@ type testStruct struct{}
 func (t testStruct) VMethod()  {}
 func (t *testStruct) PMethod() {}
 
+func (t *testStruct) Returns() (string, int, bool) {
+	return "", 0, false
+}
+
 func TestBadStruct(t *testing.T) {
 	if err := Struct(42).Error(); err != errMustBeStruct {
 		t.Fatalf("%v != %v", err, errMustBeStruct)
@@ -38,5 +42,23 @@ func TestMethod(t *testing.T) {
 		if err := Struct(&testStruct{}).Method(m).Error(); err != nil {
 			t.Fatal(err)
 		}
+	}
+}
+
+func TestBadReturns(t *testing.T) {
+	if err := Struct(&testStruct{}).Returns("", 0, false).Error(); err != errMethodNotSelected {
+		t.Fatalf("%v != %v", err, errMethodNotSelected)
+	}
+	if err := Struct(&testStruct{}).Method("Returns").Returns("", 0).Error(); err != errReturnsCount {
+		t.Fatalf("%v != %v", err, errReturnsCount)
+	}
+	if err := Struct(&testStruct{}).Method("Returns").Returns("", 0, 0).Error(); err != errReturnsType {
+		t.Fatalf("%v != %v", err, errReturnsType)
+	}
+}
+
+func TestReturns(t *testing.T) {
+	if err := Struct(&testStruct{}).Method("Returns").Returns("", 0, false).Error(); err != nil {
+		t.Fatal(err)
 	}
 }
