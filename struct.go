6455b18e82b9d221c8f858874ffa0b77f2f95e89
Nathan Osman <nathan@quickmediasolutions.com>
2018-07-21 23:05:43 -0700

    Properly handle interfaces as parameters and return values.

--- struct.go
+++ struct.go
@@ -37,7 +37,10 @@ var (
 func typeAwareComparison(t reflect.Type, v interface{}) bool {
 	vType := reflect.TypeOf(v)
 	if vType.Kind() == reflect.Ptr && vType.Implements(typeType) {
-		return t == v.(reflect.Type)
+		vType = v.(reflect.Type)
+	}
+	if t.Kind() == reflect.Interface {
+		return vType.Implements(t)
 	}
 	return t == vType
 }
